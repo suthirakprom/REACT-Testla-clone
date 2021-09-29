9a7d255fdd243b1ccd41998d96dcac024107579f
suthirakprom <suthirakprom81@gmail.com>
2021-09-29 11:35:29 +0700

    add section for the tesla

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,56 @@ import Section from "./Section"
 function Home() {
     return (
         <Container>
-            This is Home
-            <Section />
+          
+            <Section 
+                title="Model S"
+                description="Order your Model-S Online"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section 
+                title="Model Y"
+                description="Order your Model-Y Online"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section 
+                title="Model 3"
+                description="Order your Model-3 Online"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section 
+                title="Model X"
+                description="Order your Model-X Online"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section 
+                title="Lower Cost Solar Panels in America"
+                description="Money-back guarantee"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order now"
+                rightBtnText="Learn more"
+            />
+            <Section 
+                title="Solar for New Roofs"
+                description="Solar Roof Costs Less Than a new Roofs"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="Order now"
+                rightBtnText="Learn more"
+            />
+            <Section 
+                title="Accesories"
+                description=""
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop now"
+               
+            />
         </Container>
     )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import styled from 'styled-components';
 
-function Section() {
+function Section({ title, description, backgroundImg, leftBtnText, rightBtnText }) {
     return (
-        <Wrap>
+        <Wrap bgImage={ backgroundImg }>
             <ItemText>
-                <h1>Model-S</h1>
-                <p>Can be order online</p>
+                <h1>{ title }</h1>
+                <p>{ description }</p>
             </ItemText>
-            <ButtonGroup>
-                
-            </ButtonGroup>
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        { leftBtnText }
+                    </LeftButton>
+                    {rightBtnText &&
+                        <RightButton>
+                            { rightBtnText }
+                        </RightButton>
+                    }
+                    
+                </ButtonGroup>
+                <DownArrow src="/images/down-arrow.svg"/>
+            </Buttons>
         </Wrap>
     )
 }
@@ -25,7 +36,13 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image: url('images/model-s.jpg');
+   
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    background-image: ${props => `url("/images/${props.bgImage}")`}
 
 `
 const ItemText = styled.div`
@@ -36,6 +53,50 @@ const ItemText = styled.div`
 `
 
 const ButtonGroup = styled.div`
+
+    display: flex;
+    margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
+
+`
+
+const LeftButton = styled.div`
+
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+
+`
+
+const RightButton = styled(LeftButton)`
+
+    background: white;
+    opacity: 0.65;
+    color: black;
+
+`
+
+const DownArrow = styled.img`
+
+    height: 40px;
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
+
+`
+
+const Buttons = styled.div`
 
 
 
